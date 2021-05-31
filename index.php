1ccce0cd7ee25024aa8192c8e8d3f3ee8d163efa
zesionlee <39211025+zesion21@users.noreply.github.com>
2021-05-31 16:16:05 +0800

    升级至6

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006-2018 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006-2019 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
@@ -12,12 +12,13 @@
 // [ 应用入口文件 ]
 namespace think;
 
-// 加载基础文件
-require __DIR__ . './thinkphp/base.php';
+require __DIR__ . '/vendor/autoload.php';
 
-// 支持事先使用静态方法设置Request对象和Config对象
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
-header('Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS,PATCH');
-// 执行应用并响应
-Container::get('app')->run()->send();
+// 执行HTTP应用并响应
+$http = (new App())->http;
+
+$response = $http->run();
+
+$response->send();
+
+$http->end($response);
